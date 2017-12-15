459df9e544b9136aef4992031f0bcfcca4c938a9
Lancky <Lanckyblue2013@163.com>
2017-12-15 14:06:56 +0800

    删除阅读小程序的页面及逻辑

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 //app.js
 App({
   onLaunch: function () {
-    //引入SDK
-    require('./sdk-v1.1.1.js')
-    // 初始化 SDK
-    let clientID = '07109ed66fb97d68e879'
-    wx.BaaS.init(clientID)
-
-
-
-
-
     // 登录
     wx.login({
       success: res => {
