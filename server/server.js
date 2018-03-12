434108e73f207facbc37771ca283f9ea05cb9d38
aliarmo <aliarmo@tencent.com>
2018-03-12 22:49:27 +0800

    add fake data

--- server/server.js
+++ server/server.js
@@ -43,3 +43,46 @@ var router=fs.readdirSync(path.join(__dirname,'routers'))
 router.forEach(path=>{
   app.use('/', require(`./routers/${path}`))
 })
+
+var request=require('request')
+async function auto(){
+    await saveFakeData({
+      username: "axin2222222", userImg: "https://wx.qlogo.cn/mmopen/vi_32/mNCCyibfZicjaL994ACadhsJlbGLB6JR8p9diaibhiblzwMxXHeHl4oAt6afRYotqG7ibw94LqqZAu0oMfm6SUnTIJQA/0",
+      name: "读书打开，长期有效",
+      startTime: "00:00",
+      endTime: "23:59",
+      startDate: '2018-01-11',
+      endDate: '2020-12-25',
+      desc: '每天都要读书打卡'
+    })
+    await saveFakeData({
+      username: "gardon22222222",
+      userImg: "https://wx.qlogo.cn/mmopen/vi_32/mNCCyibfZicjaL994ACadhsJlbGLB6JR8p9diaibhiblzwMxXHeHl4oAt6afRYotqG7ibw94LqqZAu0oMfm6SUnTIJQA/0",
+      name: "晨跑，无效",
+      startTime: "07:00",
+      endTime: "08:00",
+      startDate: '2018-01-11',
+      endDate: '2018-02-21',
+      desc: '早起晨跑打卡'
+    })
+}
+
+function saveFakeData(data){
+  var promise=new Promise((resolve,reject)=>{
+    request.post({ url: `http://127.0.0.1:${PORT}/create`, form:data}, (err, res,body) => {
+      if (err) {
+        console.log('create fakeData error ', err)
+        reject(err)
+      } else {
+        resolve(body)
+        console.log('create fakeData success ', body)
+      }
+    })
+  })
+  return promise
+}
+
+var argv = process.argv
+if (argv.length >= 3) {
+  auto()
+}
